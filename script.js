803d38ff83fd2e12d37a86938f9a94e9d10f4040
Emanoel Pontes <albuquerqueemanoel273@gmail.com>
2024-09-09 23:08:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
         menu.classList.toggle('ativo');
         NavMenu.classList.toggle('ativo');
     });
+
 //caixa de descrição de cada produto
     const saibaMaisBtn = document.getElementById('saibaMaisBtn');
     const productDescription = document.getElementById('productDescription');
@@ -17,3 +18,36 @@
     closeBtn.addEventListener('click', function(){
         productDescription.style.display = 'none';
     });
+// caixa de descriçao do produto 02
+    const saibaMaisBtn2 = document.getElementById('saibaMaisBtn2');
+    const productDescription2 = document.getElementById('productDescription2');
+    const closeBtn2 = document.getElementById('closeBtn2');
+
+    saibaMaisBtn2.addEventListener('click', function(){
+        productDescription2.style.display = 'block';
+    })
+    closeBtn2.addEventListener('click', function(){
+        productDescription2.style.display = 'none';
+    });
+// caixa de descriçao do produto 03
+const saibaMaisBtn3 = document.getElementById('saibaMaisBtn3');
+const productDescription3 = document.getElementById('productDescription3');
+const closeBtn3 = document.getElementById('closeBtn3');
+
+saibaMaisBtn3.addEventListener('click', function(){
+    productDescription3.style.display = 'block';
+})
+closeBtn3.addEventListener('click', function(){
+    productDescription3.style.display = 'none';
+});
+// caixa de descriçao do produto 04
+const saibaMaisBtn4 = document.getElementById('saibaMaisBtn4');
+const productDescription4 = document.getElementById('productDescription4');
+const closeBtn4 = document.getElementById('closeBtn4');
+
+saibaMaisBtn4.addEventListener('click', function(){
+    productDescription4.style.display = 'block';
+})
+closeBtn4.addEventListener('click', function(){
+    productDescription4.style.display = 'none';
+});
